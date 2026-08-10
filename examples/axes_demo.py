d58cfe8cb78a6a27543983b4b59e4e09703399cc
PaoloLupo <paologuillenlupo@gmail.com>
2026-08-09 19:34:00 -0500

    mejora al axes_demo

--- examples/axes_demo.py
+++ examples/axes_demo.py
@@ -3,7 +3,7 @@
 import math
 import os
 
-from gaanim import BLACK, BLUE, GOLD, GREEN, RED, TEAL, Axis, Expr, Scene
+from gaanim import BLACK, BLUE, GOLD, GREEN, ORANGE, RED, TEAL, Axis, Brush, Expr, Scene
 
 scene = Scene(1920, 1080)
 scene.canvas.set_theme("paper")
@@ -18,14 +18,16 @@ sine = plane.plot(amplitude.expr() * x.sin()).stroke(BLUE, 4)
 parabola = plane.plot(lambda value: 0.12 * value * value - 1.2).no_fill().stroke(GREEN, 3)
 tangent = plane.tangent(lambda value: math.sin(value), 1.2, length=3.0).stroke(RED, 3)
 area = plane.area_under(lambda value: math.sin(value) , (0, math.pi), baseline=0).fill(TEAL).opacity(0.75)
+riemann = plane.riemann_sum(lambda value: math.sin(value), (-math.pi,0), rectangles=20, baseline=0).stroke(BLACK, 3).fill(Brush.linear([ORANGE, GREEN], start=(0, 0), end=(0, 200)))
 
 point = scene.dot(7).fill(RED).at_coordinate(plane.coord(1.2, math.sin(1.2)))
 title = scene.text("CoordinateSpace + Expr + Parameter").fill(BLACK).at(0, 500)
 
 scene.play([plane.create().duration(1.0), title.write().duration(0.6)])
-scene.play([sine.write().duration(0.8), parabola.create().duration(0.8)])
+scene.play([sine.write().duration(0.8), parabola.write().duration(0.8)])
 scene.play([area.fade_in().duration(0.5), tangent.create().duration(0.5), point.fade_in().duration(0.3)])
-scene.play([amplitude.animate_to(2.0).duration(1.2)])
+scene.play([riemann.create().duration(1)])
+scene.play([amplitude.animate_to(2.5).duration(1.2)])
 
 # Every layer is a real drawable and can be animated independently.
 scene.play([plane.layer("minor_grid").fade_to(0.15).duration(0.4)])
